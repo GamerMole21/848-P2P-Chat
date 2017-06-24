d900401ed7aa2468dc1796063a519c80b5830667
Alec Pullen <alecpullen24@gmail.com>
2017-06-24 19:45:32 +1000

    Fixes...

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 installdir=~/848-P2P-Chat
 sudo apt install dialog
-dialog --title "848 P2P Chat Installer" --clear --msgbox "Welcome to the 848 P2P Chat Insaller, to proceed press ENTER, to abort press ESC." 16 51
-dialog --title "848 P2P Chat Installer" --clear --msgbox "First we will update the package archives, then we will install the prerequisites." 16 51
+dialog --title "848 P2P Chat Installer" --msgbox "Welcome to the 848 P2P Chat Insaller, to proceed press ENTER, to abort press ESC." 16 51
+dialog --title "848 P2P Chat Installer" --msgbox "First we will update the package archives, then we will install the prerequisites." 16 51
 
 sudo apt update
 
@@ -13,12 +13,12 @@ sudo apt install qt-designer
 sudo apt install git
 cd ~
 cd "$installdir" || {
-dialog --title "848 P2P Chat Installer" --clear --msgbox "Cloning from GitHub..." 16 51
+dialog --title "848 P2P Chat Installer" --msgbox "Cloning from GitHub..." 16 51
 
 git clone https://github.com/GamerMole21/848-P2P-Chat.git
 cd 848-P2P-Chat
 chmod +x chat4.py
-dialog --title "848 P2P Chat Installer" --clear --msgbox "848 P2P Chat successfully installed." 16 51
+dialog --title "848 P2P Chat Installer" --msgbox "848 P2P Chat successfully installed." 16 51
 exit
 }
-dialog --title "848 P2P Chat Installer" --clear --msgbox "848 P2P Chat was already installed."
+dialog --title "848 P2P Chat Installer" --msgbox "848 P2P Chat was already installed."
