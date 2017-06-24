89e1d0a3bb61045f2d726c93366c5c8a9bc20697
Alec Pullen <alecpullen24@gmail.com>
2017-06-24 19:36:56 +1000

    Even more fixes...

--- installer.sh
+++ installer.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 installdir=~/848-P2P-Chat
 sudo apt install dialog
-dialog --title "848 P2P Chat Installer" --clear --msgbox "Welcome to the 848 P2P Chat Insaller, to proceed press ENTER, to abort press ESC?" 16 51
+dialog --title "848 P2P Chat Installer" --clear --msgbox "Welcome to the 848 P2P Chat Insaller, to proceed press ENTER, to abort press ESC." 16 51
 dialog --title "848 P2P Chat Installer" --clear --msgbox "First we will update the package archives, then we will install the prerequisites." 16 51
+clear
 sudo apt update
+clear
 sudo apt install pyqt4-dev-tools python-qt4 python3-qt4 qt-designer git
 cd ~
-if [ -d "$installdir"] then
+if [ -d "$installdir"]; then
   dialog --title "848 P2P Chat Installer" --clear --msgbox "848 P2P Chat is already installed." 16 51
 fi
 dialog --title "848 P2P Chat Installer" --clear --msgbox "Cloning from GitHub..." 16 51
+clear
 git clone https://github.com/GamerMole21/848-P2P-Chat.git
 cd 848-P2P-Chat
 chmod +x chat4.py
